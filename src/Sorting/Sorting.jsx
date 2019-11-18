ad8debca3c4323e64d8d2ab7fb030dd3e34f6cda
space <gettubby55@gmail.com>
2019-11-18 14:46:53 -0800

    kko

--- src/Sorting/Sorting.jsx
+++ src/Sorting/Sorting.jsx
@@ -16,21 +16,25 @@ export default class Sorting extends Component {
 
   resetArray() {
     const array = [];
-    for (let i = 0; i < 20; ++i) array.push(randomize(1, 21));
+    for (let i = 0; i < 10; ++i) array.push(randomize(1, 10));
     this.setState({ array });
   }
 
   mergeSort() {
-    //const { array } = this.state;
+    const { array } = this.state;
     const animations = getAnimations(this.state.array);
 
     for (let i = 0; i < animations.length; ++i) {
       const arrayBars = document.getElementsByClassName("array-bar");
       setTimeout(() => {
         const [barOne, newValue] = animations[i];
-        console.log(barOne, newValue);
+        //console.log(barOne, newValue);
+        //array[barOne] = newValue;
+        this.setState({array});
         arrayBars[barOne].style.value = newValue;
-      }, i * 3);
+        //arrayBars[barOne].textContent = newValue;
+        //this.setState({array});
+      }, i * 1);
     }
   }
 
@@ -49,7 +53,6 @@ export default class Sorting extends Component {
                 backgroundColor: "orange" /*, height: `${value}px`*/
               }}
             >
-              {" "}
               {value}{" "}
             </div>
           ))}
